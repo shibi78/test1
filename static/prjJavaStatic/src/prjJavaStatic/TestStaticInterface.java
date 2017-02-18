4b80834c28798766bd474ee0222da6524d9d138e
shibi <shibi@192.168.0.109>
2017-02-18 10:14:16 -0800

    my commit

---
+++ static/prjJavaStatic/src/prjJavaStatic/TestStaticInterface.java
@@ -0,0 +1,31 @@
+package prjJavaStatic;
+
+interface Drawable
+{
+	void draw();
+	static int cube(int x) {
+		return x*x*x;
+	}
+}
+
+class rectangle1 implements Drawable
+{
+	public void draw(){
+		System.out.println("drawing rectangle");
+		System.out.println("drawing rectangle");
+		System.out.println("drawing rectangle");
+		
+	}
+}
+
+public class TestStaticInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Drawable d=new rectangle1();
+		d.draw();
+		//System.out.println(Drawable.cube(10));
+
+	}
+
+}
